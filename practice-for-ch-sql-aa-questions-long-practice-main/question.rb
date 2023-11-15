f7f988c8565834e18d3e949955b3ce023aebd410
Jacob Tabor <jtabor214@gmail.com>
2023-11-15 17:08:04 -0500

    Created #initializes, established attr_accessors and attr_reader

--- practice-for-ch-sql-aa-questions-long-practice-main/question.rb
+++ practice-for-ch-sql-aa-questions-long-practice-main/question.rb
@@ -2,6 +2,9 @@ class Question
 
     require 'questions_database'
 
+    attr_accessor :title, :body, :author_id
+    attr_reader :id
+
     def self.find_by_id(id)
         question = QuestionsDatabase.instance.execute(<<-SQL, id)
             SELECT
@@ -24,6 +27,13 @@ class Question
                 author_id = ?
         SQL
         question_asker.map{|question| Question.new(question)}
+    end
+
+    def initialize(options)
+        @id = options['id']
+        @title = options['title']
+        @body = options['body']
+        @author_id = options['author_id']
     end
 
 end
