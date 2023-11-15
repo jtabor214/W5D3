b904abeb3ca00ce57f0610b4c26e0a77b290cd79
Jacob Tabor <jtabor214@gmail.com>
2023-11-15 16:26:51 -0500

    Debugged in order to establish working SQL file and generate tables

--- practice-for-ch-sql-aa-questions-long-practice-main/import_db.sql
+++ practice-for-ch-sql-aa-questions-long-practice-main/import_db.sql
@@ -1,54 +1,57 @@
-PRAGMA foreign_keys = ON; 
+PRAGMA foreign_keys = 0; 
 
-    DROP TABLE IF EXISTS users;
-    CREATE TABLE users (
-        id INTEGER PRIMARY KEY, 
-        fname TEXT NOT NULL,
-        lname TEXT NOT NULL,
+    CREATE TABLE users ( 
+    id INTEGER PRIMARY KEY, 
+    fname TEXT NOT NULL, 
+    lname TEXT NOT NULL
     );
 
     DROP TABLE IF EXISTS questions;
-    CREATE TABLE questions (
-        id INTEGER PRIMARY KEY,
-        title TEXT NOT NULL,
-        body TEXT NOT NULL, 
-        author_id INTEGER NOT NULL,
 
-        FOREIGN KEY (user_id) REFERENCES users(id)
+    CREATE TABLE questions ( 
+    id INTEGER PRIMARY KEY, 
+    title TEXT NOT NULL, 
+    body TEXT NOT NULL, 
+    author_id INTEGER NOT NULL, 
+
+    FOREIGN KEY (author_id) REFERENCES users(id)
     );
 
     DROP TABLE IF EXISTS question_follows;
-    CREATE TABLE question_follows (
-        id INTEGER PRIMARY KEY, 
-        user_id INTEGER PRIMARY KEY,
-        question_id INTEGER PRIMARY KEY,
 
-        FOREIGN KEY (user_id) REFERENCES users(id),
-        FOREIGN KEY (question_id) REFERENCES question(id)
+    CREATE TABLE question_follows ( 
+    id INTEGER PRIMARY KEY, 
+    user_id INTEGER NOT NULL, 
+    question_id INTEGER NOT NULL, 
+
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (question_id) REFERENCES questions(id)
     );
 
-    DROP TABLE IF EXISTS replies 
-    CREATE TABLE replies (
-        id INTEGER PRIMARY KEY, 
-        parent_reply_id INTEGER, 
-        user_id INTEGER,
-        body TEXT NOT NULL,
-        subject_question_id INTEGER NOT NULL,
+    DROP TABLE IF EXISTS replies;
 
-        FOREIGN KEY (subject_question_id) REFERENCES questions(id),
-        FOREIGN KEY (user_id) REFERENCES users(id),
-        FOREIGN KEY (parent_reply_id) REFERENCES replies(id)
+    CREATE TABLE replies ( 
+    id INTEGER PRIMARY KEY,
+    body TEXT NOT NULL, 
+    subject_question_id INTEGER NOT NULL,
+    user_id INTEGER NOT NULL, 
+    parent_reply_id INTEGER,
+
+    FOREIGN KEY (subject_question_id) REFERENCES questions(id),
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (parent_reply_id) REFERENCES replies(id)
     );
 
-    DROP TABLE IF EXISTS question_likes;
-    CREATE TABLE question_likes (
-        id INTEGER PRIMARY KEY,
-        user_id INTEGER PRIMARY KEY,
-        question_id INTEGER PRIMARY KEY,
+    DROP TABLE IF EXISTS question_likes; 
 
-        FOREIGN KEY (user_id) REFERENCES users(id),
-        FOREIGN KEY (question_id) REFERENCES questions(id)
-    );
+    CREATE TABLE question_likes ( 
+    id INTEGER PRIMARY KEY, 
+    user_id INTEGER NOT NULL, 
+    question_id INTEGER NOT NULL, 
+
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (question_id) REFERENCES questions(id)
+    ); 
 
     INSERT INTO 
     users (fname, lname)
@@ -59,21 +62,21 @@ PRAGMA foreign_keys = ON;
         ('Ishan', 'Chawla');
     
     INSERT INTO 
-    question (title, body, author_id)
+    questions (title, body, author_id)
     VALUES 
-        ('Q1', 'Who''s that Pokemon?', 1)
-        ('Q2', 'Why am I with these people?', 2)
-        ('Q3', 'Banana?', 3)
-        ('Q4', 'Gggrreeeukiiee?', 3)
+        ('Q1', 'Who''s that Pokemon?', 1),
+        ('Q2', 'Why am I with these people?', 2),
+        ('Q3', 'Banana?', 3),
+        ('Q4', 'Gggrreeeukiiee?', 3),
         ('Q5', 'Were you tooo fast?', 4);
 
     INSERT INTO 
     question_follows (user_id, question_id)
     VALUES 
-        (2, 1)
-        (1, 4)
-        (3, 3)
-        (4, 1)
+        (2, 1),
+        (1, 4),
+        (3, 3),
+        (4, 1);
 
     INSERT INTO
     replies (body, subject_question_id, user_id, parent_reply_id)
