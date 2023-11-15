22baf272b4b1346ef94130aad20369741880f7a4
Jacob Tabor <jtabor214@gmail.com>
2023-11-15 17:22:43 -0500

    Created ::self.find_by_id and ::self.find_by_name for User class

---
+++ practice-for-ch-sql-aa-questions-long-practice-main/users.rb
@@ -0,0 +1,30 @@
+class Users
+    require_relative 'questions_database'
+    require_relative 'question'
+    require_relative 'reply' 
+    require_relative 'question_follows'
+
+    def self.find_by_id(id)
+        users = QuestionsDatabase.instance.execute(<<-SQL, id)
+            SELECT
+                *
+            FROM 
+                users
+            WHERE 
+                id = ?
+        SQL
+        users.map{|user| User.new(user)}
+    end
+
+    def self.find_by_name(fname, lname)
+        user = QuestionsDatabase.instance.execute(<<-SQL, id)
+            SELECT
+                *
+            FROM 
+                users
+            WHERE 
+                fname = ? AND lname = ?
+        SQL
+        User.new(user.first)
+    end
+end
