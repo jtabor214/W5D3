a1aad11525a5207149b95a210b17cada27c6daed
Jacob Tabor <jtabor214@gmail.com>
2023-11-15 15:23:08 -0500

    Extended SQL file by adding question_follows, replies, question likes, tables

--- practice-for-ch-sql-aa-questions-long-practice-main/import_db.sql
+++ practice-for-ch-sql-aa-questions-long-practice-main/import_db.sql
@@ -16,3 +16,36 @@ PRAGMA foreign_keys = ON;
 
         FOREIGN KEY (user_id) REFERENCES users(id)
     );
+
+    DROP TABLE IF EXISTS question_follows;
+    CREATE TABLE question_follows (
+        id INTEGER PRIMARY KEY, 
+        user_id INTEGER PRIMARY KEY,
+        question_id INTEGER PRIMARY KEY,
+
+        FOREIGN KEY (user_id) REFERENCES users(id),
+        FOREIGN KEY (question_id) REFERENCES question(id)
+    );
+
+    DROP TABLE IF EXISTS replies 
+    CREATE TABLE replies (
+        id INTEGER PRIMARY KEY, 
+        parent_reply_id INTEGER, 
+        user_id INTEGER,
+        body TEXT NOT NULL,
+        subject_question_id INTEGER NOT NULL,
+
+        FOREIGN KEY (subject_question_id) REFERENCES questions(id),
+        FOREIGN KEY (user_id) REFERENCES users(id),
+        FOREIGN KEY (parent_reply_id) REFERENCES replies(id)
+    );
+
+    DROP TABLE IF EXISTS question_likes;
+    CREATE TABLE question_likes (
+        id INTEGER PRIMARY KEY,
+        user_id INTEGER PRIMARY KEY,
+        question_id INTEGER PRIMARY KEY,
+
+        FOREIGN KEY (user_id) REFERENCES users(id),
+        FOREIGN KEY (question_id) REFERENCES questions(id)
+    );
